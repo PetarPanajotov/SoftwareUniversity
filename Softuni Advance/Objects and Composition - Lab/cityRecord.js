bd0ca662c3b8de0af479f4507feb06d6416b900c
PetarPanajotov <petarpanajotov@gmail.com>
2022-09-21 20:14:30 +0300

    Solved problem with factory functions.

--- Softuni Advance/Objects and Composition - Lab/cityRecord.js
+++ Softuni Advance/Objects and Composition - Lab/cityRecord.js
@@ -1,3 +1,4 @@
+// You will receive a city’s name (string), population (number), and treasury (number) as arguments, which you will need to set as properties of an object and return it
 function cityRecord (city, population, treasury) {
     let cityInfo =  {'name': city,
     'population': population,
