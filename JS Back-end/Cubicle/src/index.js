c17c36649292436a3fb3ecbc0c584fc85b63e815
Petar Panayotov <dakona98@gmail.com>
2023-01-24 16:13:04 +0200

    Configurated express-handlebars, added css and favicon.

--- JS Back-end/Cubicle/src/index.js
+++ JS Back-end/Cubicle/src/index.js
@@ -1,11 +1,15 @@
 const env = process.env.NODE_ENV || 'development';
 
 const express = require('express');
-const config = require('../config/config')[env]
-const app = express();
+const config = require('./config/config')[env];
+const setupView = require('./config/viewEngine');
 
+const app = express();
+setupView(app)
+
+app.use(express.static('src/public'));
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    res.render('index');
+});
 
 app.listen(config.port, console.log(`Server listening on port ${config.port}`));
