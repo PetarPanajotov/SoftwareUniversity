67ea9e708d7b5fce20b121456f3a918aa985dd08
Petar Panayotov <dakona98@gmail.com>
2023-01-24 16:19:40 +0200

    Added router with dummy views.

--- JS Back-end/Cubicle/src/index.js
+++ JS Back-end/Cubicle/src/index.js
@@ -11,5 +11,14 @@ app.use(express.static('src/public'));
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/deails', (req, res) => {
+    res.render('details');
+});
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(config.port, console.log(`Server listening on port ${config.port}`));
