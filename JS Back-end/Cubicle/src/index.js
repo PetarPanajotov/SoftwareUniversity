c714ee287e49c28874569f674eb1604e999360c7
Petar Panayotov <dakona98@gmail.com>
2023-01-24 14:47:11 +0200

    Server setup

--- JS Back-end/Cubicle/src/index.js
+++ JS Back-end/Cubicle/src/index.js
@@ -1,1 +1,11 @@
-console.log('hola')
+const env = process.env.NODE_ENV || 'development';
+
+const express = require('express');
+const config = require('../config/config')[env]
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(config.port, console.log(`Server listening on port ${config.port}`));
