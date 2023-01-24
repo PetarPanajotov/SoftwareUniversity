019955888e0005ef7c3525eed82c8a8c43b24c79
Petar Panayotov <dakona98@gmail.com>
2023-01-24 16:34:21 +0200

    Modified application architecture. Added modular router.

--- JS Back-end/Cubicle/src/index.js
+++ JS Back-end/Cubicle/src/index.js
@@ -3,22 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 const express = require('express');
 const config = require('./config/config')[env];
 const setupView = require('./config/viewEngine');
+const routes = require('./routes');
 
 const app = express();
 setupView(app)
 
 app.use(express.static('src/public'));
-app.get('/', (req, res) => {
-    res.render('index');
-});
-app.get('/deails', (req, res) => {
-    res.render('details');
-});
-app.get('/create', (req, res) => {
-    res.render('create');
-});
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.use(routes);
 
 app.listen(config.port, console.log(`Server listening on port ${config.port}`));
