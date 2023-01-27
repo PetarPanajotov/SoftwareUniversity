286baafed9e6ccf8c86c4fbcd468cd52812fa0ff
Petar Panayotov <dakona98@gmail.com>
2023-01-27 20:56:35 +0200

    Update config.js

--- JS Back-end/Cubicle/src/config/config.js
+++ JS Back-end/Cubicle/src/config/config.js
@@ -1,8 +1,11 @@
-module.exports = {
+const config = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        DB_URI: 'mongodb://127.0.0.1:27017/cubicle'
     },
     production: {
-        port: process.env.PORT || 5000
+        port: process.env.PORT || 5000,
+        DB_URI: 'mongodb://127.0.0.1:27017/cubicle'
     }
 };
+module.exports = config[process.env.node_env || 'development']
