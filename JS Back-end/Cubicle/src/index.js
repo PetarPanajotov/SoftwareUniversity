5cc41986c19bb35d1b381b75e0530b032a81c938
Petar Panayotov <dakona98@gmail.com>
2023-02-02 14:52:59 +0200

    Add JWT and cookie-parser;

--- JS Back-end/Cubicle/src/index.js
+++ JS Back-end/Cubicle/src/index.js
@@ -3,13 +3,16 @@ const config = require('./config/config');
 const setupView = require('./config/viewEngine');
 const routes = require('./routes');
 const databaseInit = require('./config/databaseConfig')
+const cookieParser = require('cookie-parser')
 
 const app = express();
+app.use(cookieParser());
 setupView(app)
 
 app.use(express.static('src/public'));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
+
 databaseInit()
     .then(() => app.listen(config.port, console.log(`Server listening on port ${config.port}`)))
     .catch((err) => console.error(err.message));
