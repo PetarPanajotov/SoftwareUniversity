2a407f0363f9bda4b4f184552ec8b47f394c6251
Petar Panayotov <dakona98@gmail.com>
2023-02-13 16:04:00 +0200

    added authentication middleware to check

--- JS Back-end/Crypto Trade/src/index.js
+++ JS Back-end/Crypto Trade/src/index.js
@@ -3,12 +3,16 @@ const handlebarsConfig = require('./configs/viewEngine');
 const databaseInit = require('./configs/databaseSetup');
 const express = require('express');
 const router = require('./routes');
+const { authentication } = require('./middlewares/authMiddleware');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
+app.use(cookieParser());
 
 app.use(urlencoded({extended: false}));
 app.use(express.static('src/public'));
 handlebarsConfig(app);
+app.use(authentication);
 app.use(router);
 
 databaseInit()
---
+++ JS Back-end/Crypto Trade/src/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('../utils/jwtUtils');
+
+exports.authentication = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, 'Xen');
+
+            req.user = decodedToken;
+            req.isAuthenticated = true;
+
+            res.locals.username = decodedToken.username;
+            res.locals.isAuthenticated = true;
+        } catch (err) {
+            console.log(err.message);
+            res.clearCookie('auth');
+            res.render('404')
+        };
+    };
+    next();
+};
