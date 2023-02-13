3732193ecf3e6ec3db0e8c7964c5fb404ac583e2
Petar Panayotov <dakona98@gmail.com>
2023-02-13 15:10:51 +0200

    Added dynamic catalog and create saves correct in DB

--- JS Back-end/Crypto Trade/src/controllers/cryptoController.js
+++ JS Back-end/Crypto Trade/src/controllers/cryptoController.js
@@ -1,6 +1,17 @@
-exports.getCatalogPage = (req, res) => {
-    res.render('catalog');
+const Crypto = require('../models/Crypto');
+
+exports.getCatalogPage = async (req, res) => {
+    const cryptos = await Crypto.find().lean();
+    res.render('catalog', {cryptos});
+
 };
 exports.getCreatePage = (req, res) => {
     res.render('create');
+};
+
+exports.postCrypto = async (req, res) => {
+    const { name, imageUrl, price, description, payment } = req.body;
+    const crypto = new Crypto({ name, imageUrl, price, description, payment});
+    await crypto.save();
+    res.redirect('/catalog');
 };
