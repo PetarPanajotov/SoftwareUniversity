734df70388ebb59cee762ae9cfd55c6ab37a1bd7
Petar Panayotov <dakona98@gmail.com>
2023-02-13 14:19:12 +0200

    register now correct makes account in database.

--- JS Back-end/Crypto Trade/src/controllers/authController.js
+++ JS Back-end/Crypto Trade/src/controllers/authController.js
@@ -1,3 +1,14 @@
-exports.getRegister = (req,res) => {
+const { register } = require("../services/authService");
+
+exports.getRegister = (req, res) => {
     res.render('register')
 }
+exports.postRegister = async (req, res) => {
+    const { username, email, password, confirmPassword } = req.body;
+    try {
+        await register(username, email, password, confirmPassword);
+    } catch (err) {
+        console.log(err)
+    }
+    res.redirect('/')
+};
