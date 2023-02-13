b2577e48ecdc65f7513f374ad597eb7820d88372
Petar Panayotov <dakona98@gmail.com>
2023-02-13 14:19:24 +0200

    Update routes.js

--- JS Back-end/Crypto Trade/src/routes.js
+++ JS Back-end/Crypto Trade/src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { getRegister, postRegister } = require('./controllers/authController');
 const { getHomePage } = require('./controllers/homeController');
 const router = express.Router();
 
 router.get('/', getHomePage);
+router.get('/register', getRegister);
+router.post('/register', postRegister);
 
 module.exports = router;
