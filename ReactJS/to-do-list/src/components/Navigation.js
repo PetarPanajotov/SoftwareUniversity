e55d5829c3f545d7188cfec3aede621dd9250ee0
Petar Panayotov <dakona98@gmail.com>
2023-03-08 17:09:55 +0200

    Added navigation.

---
+++ ReactJS/to-do-list/src/components/Navigation.js
@@ -0,0 +1,11 @@
+export function Navigation() {
+    return (
+        <header class="navigation-header">
+            <span class="navigation-logo">
+                <img src="./static/images/todo-icon.png" alt="todo-logo"></img>
+            </span>
+            <span class="spacer"></span>
+            <span class="navigation-description">Todo List</span>
+        </header>
+    )
+}
