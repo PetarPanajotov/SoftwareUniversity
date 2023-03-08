0450f937978a788dc494daf15d845448eb8515d2
Petar Panayotov <dakona98@gmail.com>
2023-03-08 18:40:54 +0200

    Update Table.js

--- ReactJS/to-do-list/src/components/Table.js
+++ ReactJS/to-do-list/src/components/Table.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react"
+const url = 'http://localhost:3030/jsonstore/todos'
+
 export function Table() {
+    const [task, setTask] = useState([])
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                setTask(Object.values(data))
+            })
+    }, [])
+    console.log(task)
     return (
         <table className="table">
             <thead>
@@ -8,15 +21,18 @@ export function Table() {
                     <th className="table-header-action">Action</th>
                 </tr>
             </thead>
+            
             <tbody>
-                <tr class="todo is-completed">
-                    <td>Give dog a bath</td>
-                    <td>Complete</td>
+                {task.map(movie =>
+                <tr className= {movie.isCompleted? "todo is-completed": "todo"}>
+                    <td>{movie.text}</td>
+                    <td>{movie.isCompleted? 'Completed': 'Incomplete'}</td>
                     <td className="todo-action">
                         <button className="btn todo-btn">Change status</button>
                     </td>
                 </tr>
+                )}
             </tbody>
         </table>
     )
-}
+};
