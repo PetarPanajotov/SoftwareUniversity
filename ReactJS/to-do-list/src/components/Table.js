ccbd07367c1ba52f0fdf9481caaf36db9abec685
Petar Panayotov <dakona98@gmail.com>
2023-03-08 19:28:37 +0200

    Added load spinner while fetching.

--- ReactJS/to-do-list/src/components/Table.js
+++ ReactJS/to-do-list/src/components/Table.js
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react"
+import { LoadSpinner } from "./LoadSpinner";
 const url = 'http://localhost:3030/jsonstore/todos'
 
 export function Table() {
-    const [task, setTask] = useState([])
+    const [task, setTask] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch(url)
             .then(response => response.json())
             .then((data) => {
                 setTask(Object.values(data))
+                setLoading(false)
             })
     }, [])
-    console.log(task)
+
+    
     function changeStatus(e) {
-        debugger;
         const updateTask = task.map(item => {
-            debugger;
             if (item._id === e.target.id) {
                 item.isCompleted = !item.isCompleted
             }
@@ -24,26 +26,29 @@ export function Table() {
         setTask(updateTask);
     }
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th className="table-header-task">Task</th>
-                    <th className="table-header-status">Status</th>
-                    <th className="table-header-action">Action</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {task.map(task =>
-                    <tr className={task.isCompleted ? "todo is-completed" : "todo"} key={task._id}>
-                        <td>{task.text}</td>
-                        <td>{task.isCompleted ? 'Completed' : 'Incomplete'}</td>
-                        <td className="todo-action">
-                            <button className="btn todo-btn" onClick={changeStatus} id={task._id}>Change status</button>
-                        </td>
+        <>
+            {loading ? <LoadSpinner /> : null}
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th className="table-header-task">Task</th>
+                        <th className="table-header-status">Status</th>
+                        <th className="table-header-action">Action</th>
                     </tr>
-                )}
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    {task.map(task =>
+                        <tr className={task.isCompleted ? "todo is-completed" : "todo"} key={task._id}>
+                            <td>{task.text}</td>
+                            <td>{task.isCompleted ? 'Completed' : 'Incomplete'}</td>
+                            <td className="todo-action">
+                                <button className="btn todo-btn" onClick={changeStatus} id={task._id}>Change status</button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </>
     )
 };
