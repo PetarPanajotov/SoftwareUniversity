71674170f653badae1b7db5e1c1c538546ee1a3a
Petar Panayotov <dakona98@gmail.com>
2023-03-08 17:23:32 +0200

    Added load spinner and "add new todo" button.

--- ReactJS/to-do-list/src/App.js
+++ ReactJS/to-do-list/src/App.js
@@ -1,9 +1,22 @@
+import { AddNewTodo } from './components/AddNewTodo';
+import { LoadSpinner } from './components/LoadSpinner';
 import { Navigation } from './components/Navigation';
 
 function App() {
-  return (
-  <Navigation />
-  )
+    return (
+        <>
+            <Navigation />
+            <main className="main">
+                <section className="todo-list-container">
+                    <h1>Todo List</h1>
+                    <AddNewTodo />
+                    <div className="table-wrapper">
+                        <LoadSpinner />
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default App;
---
+++ ReactJS/to-do-list/src/components/AddNewTodo.js
@@ -0,0 +1,7 @@
+export function AddNewTodo() {
+    return (
+        <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+        </div>
+    )
+}
