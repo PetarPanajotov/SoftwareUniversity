dd920e994dca714b567250799bac631188dbd56c
Petar Panayotov <dakona98@gmail.com>
2023-03-09 18:38:35 +0200

    Added footer and pagination.

--- ReactJS/user-lists/src/App.js
+++ ReactJS/user-lists/src/App.js
@@ -1,4 +1,6 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Pagination } from "./components/Pagination";
 import { Search } from "./components/Search";
 import { TableList } from "./components/TableList";
 function App() {
@@ -8,12 +10,11 @@ function App() {
             <main className="main">
                 <section className="card users-container">
                     <Search />
-                    <div className="table-wrapper">
-                        <TableList />
-                    </div>
-                        <button className="btn-add btn">Add new user</button>
+                    <TableList />
+                    <Pagination />
                 </section>
             </main>
+            <Footer />
         </>
     )
 }
