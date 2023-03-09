8f437ad2b903955f546c68d4b6d605bee9458c80
Petar Panayotov <dakona98@gmail.com>
2023-03-09 18:31:44 +0200

    Added table component from resources.

--- ReactJS/user-lists/src/App.js
+++ ReactJS/user-lists/src/App.js
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header";
 import { Search } from "./components/Search";
-
+import { TableList } from "./components/TableList";
 function App() {
     return (
         <>
@@ -8,6 +8,10 @@ function App() {
             <main className="main">
                 <section className="card users-container">
                     <Search />
+                    <div className="table-wrapper">
+                        <TableList />
+                    </div>
+                        <button className="btn-add btn">Add new user</button>
                 </section>
             </main>
         </>
